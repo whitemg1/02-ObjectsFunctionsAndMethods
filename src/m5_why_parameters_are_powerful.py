2c78b1165cb2a0e703d32e2a474cf3853b5e3072
whitemg1 <whitemg1@rose-hulman.edu>
2018-11-29 09:19:52 -0500

    DONE

--- src/m5_why_parameters_are_powerful.py
+++ src/m5_why_parameters_are_powerful.py
@@ -198,7 +198,7 @@ def even_better_draw_circles(point, delta_r, num_of_circles, pen_color, pen_thic
         turtle.draw_circle(delta_r * k)  # Radius 15, 30, 45, 60, ...
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #
 # Finally, comment-out the existing calls to  even_better_draw_circles  and
 # add code in   main  to draw various circles that form a BEAUTIFUL picture!
